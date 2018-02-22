c035bcdefdda1f4e3fd6ddd53be2ac628dde050f
joannejc <joannejc5000@gmail.com>
2018-02-22 13:48:03 -0800

    CH2 setup

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 from app import app
+from flask import render_template
 import time
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'wow joanne and gary are so cool'
+    user = {'username': 'Hippo',
+            'networth': '10B'}
+    posts = [
+        {
+            'author': {'username': 'Hippo kid'},
+            'body': 'I was born last week!'
+        },
+        {
+            'author': {'username': 'Hippo baby'},
+            'body': 'I was born last night..'
+        }
+    ]
+    return render_template('index.html', title = "HOME", user = user, posts = posts)
